96d77917b9acf611aad383d9b3db0ee9232fd448
Mike Iversen <miversen33@gmail.com>
2023-04-16 12:08:37 -0500

    Added Default commands and nav pane

--- lua/neo-tree-dummy-extension/init.lua
+++ lua/neo-tree-dummy-extension/init.lua
@@ -3,11 +3,17 @@ if vim.g.neo_tree_dummy_extension_no_name then
     name = nil
 end
 
+local commands = {}
+require("neo-tree.sources.common.commands")._add_common_commands(commands)
+
 local M = {
     name = name,
+    display_name = 'ﭧ Dummy',
     components = {},
-    commands = {},
-    navigate = function() end,
+    commands = commands,
+    navigate = function(state)
+        require("neo-tree.ui.renderer").show_nodes({}, state)
+    end,
     setup = function() end,
 }
 
